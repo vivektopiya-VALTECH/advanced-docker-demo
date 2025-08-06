ce35180e8b5e4a08bec66ae93cf0d76e9ffb10b6
Vivek Topiya <vivek.topiya@valtech.com>
2025-08-06 13:44:04 +0530

    initial commit

---
+++ src/main/java/com/cap/advanceddockerdemo/AdvancedDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cap.advanceddockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedDockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedDockerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cap/advanceddockerdemo/AdvancedDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.advanceddockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedDockerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
